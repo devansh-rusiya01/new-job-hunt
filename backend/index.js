d431c8b8e0b4f6315926f4e350f3313792bd7a85
devansh-rusiya01 <devanshrusiya8@gmail.com>
2024-09-29 18:51:23 +0530

    fin2

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:"https://new-job-hunt-1-o.onrender.com",
     credentials:true
 }
 app.use(cors(corsOptions));
